8da99cb98ed0614f67bcdb49c42cadf87e7fed35
Manel <ea3eiz@ea3eiz.com>
2020-04-06 01:11:31 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-ultima=6.0.3
+ultima=6.0.4
 actualizacion=$(awk "NR==2" /var/www/html/comprueba_actualizacion.php)
 if [ $actualizacion = $ultima ];
 then
@@ -10,15 +10,16 @@ else
 
 #sed -i '201c <li><a href="##">V<?php echo $actualizacion;?></a></li>' /var/www/html/panel_configuracion.php
 
-#sudo cp /var/www/html/Actualizar_DVSWITCH_LITE/panel_configuracion.php /var/www/html
+#========================================================================================
+# Cambios 06-04-2020
+sudo cp /var/www/html/Actualizar_DVSWITCH_LITE/panel_configuracion.php /var/www/html
+sudo cp /var/www/html/Actualizar_DVSWITCH_LITE/sistema_plus.php /var/www/html
+sudo cp /var/www/html/Actualizar_DVSWITCH_LITE/cambia_options_dmrplus.php /var/www/html
+#========================================================================================
 
 
-#dmrplus_21465.sh
-
-sudo sed -i "58c RAN=1" /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini
-
+# Estas lineas siempre que haya actualización, se deben de escribir
 sudo sed -i "2c $ultima" /var/www/html/comprueba_actualizacion.php
-
 sudo sed -i "3c Actualizando a la V-$ultima" /var/www/html/comprueba_actualizacion.php
 
 fi
